98c65c902cac466f2510227c1ba5dcfc951f2066
LordGaaju001 <chrisgaajukpeme2027@gmail.com>
2026-02-12 17:09:07 +0100

    Update textmaker.js

--- commands/textmaker.js
+++ commands/textmaker.js
@@ -6,8 +6,8 @@ const channelInfo = {
     forwardingScore: 1,
     isForwarded: true,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363406735242612@newsletter',
-        newsletterName: 'Zenitsu-BOT',
+        newsletterJid: '120363406588763460@newsletter',
+        newsletterName: 'Zenitsu-Bot',
         serverMessageId: -1
     }
 };
